b6204c374b6bfce102bdb11d4474863a0aa69fb9
Matt ‘TK’ Taylor <mattietk@gmail.com>
2022-01-10 11:37:01 +0800

    add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,20 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
+import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PlausibleProvider
+      domain="lft.tk.gg"
+      customDomain="https://stats.tk.gg"
+      selfHosted={true}
+      enabled={process.env.VERCEL_ENV == "production"}
+    >
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PlausibleProvider>
   );
 }
 
